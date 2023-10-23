103c7ff55fbc642713a69abf93708c6e22549e38
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-23 16:10:03 -0400

    Change the name of the table: 'messages'. New name: 'greetings'

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_23_194010) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_23_200238) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "messages", force: :cascade do |t|
+  create_table "greetings", force: :cascade do |t|
     t.string "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
