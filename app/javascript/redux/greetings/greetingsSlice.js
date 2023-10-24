86975a628c96f1c22387b2a8885b7561dc2d672f
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-23 21:12:09 -0400

    Create the greetingSlice.js file with the async request to the API

---
+++ app/javascript/redux/greetings/greetingsSlice.js
@@ -0,0 +1,16 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const apiEndpoint = '/api/v1/greetings/random';
+
+export const getRandomGreeting = createAsyncThunk(
+  'greetings/getRandomGreeting',
+  async (thunkAPI) => {
+    try {
+      const response = await axios.get(apiEndpoint);
+      return response.data
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  },
+);
