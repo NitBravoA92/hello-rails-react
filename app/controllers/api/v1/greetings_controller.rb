ad0a8c561d7b425aaaea06794f535a92078a2964
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-23 21:39:36 -0400

    Remove spacing and change comments

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::GreetingsController < ActionController::API
-  # get a random greeting from the greeting
+  # get a random greeting
   def random_greeting
     greeting = Greeting.order('RANDOM()').first
     render json: { message: greeting.text }
