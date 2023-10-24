ee02c414d8e3c0dbe680a0bb2e2228819cee8124
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-23 21:19:17 -0400

    Add the redux store configuration in the App component

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import store from '../redux/store';
 import Greeting from './Greeting';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Greeting />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Greeting />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
