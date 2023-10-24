05083b798c61d737cbeff1694758da91dd10e85b
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-23 21:21:49 -0400

    Display the data from the API using redux hooks

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,7 +1,38 @@
+import { useDispatch, useSelector } from 'react-redux';
 import React from 'react';
+import { useEffect } from 'react';
+import { getRandomGreeting } from '../redux/greetings/greetingsSlice';
 
 function Greeting() {
-  return (<h1>Hello This is a greeting!</h1>);
+  const {
+    greetings, isLoading, error,
+  } = useSelector((state) => state.greetings);
+
+  const dispatchActions = useDispatch();
+
+  useEffect(() => {
+    dispatchActions(getRandomGreeting());
+  }, [dispatchActions]);
+
+  let loadMessage = null;
+
+  if (isLoading) {
+    loadMessage = 'Loading message...';
+  }
+
+  if (error) {
+    loadMessage = 'Error loading data';
+  }
+
+  return (
+    <section>
+    {
+      (isLoading || error)
+        ? (<p className="status">{loadMessage}</p>)
+        : (<p class="greeting-message">{greetings?.message}</p>)
+    }
+    </section>
+  );
 }
 
 export default Greeting;
